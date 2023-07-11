f4544e7662abff33ee77e2d32c02b28755fe7823
AdvertisingMercenary <81523705+AdvertisingMercenary@users.noreply.github.com>
2023-07-11 18:40:26 -0500

    data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+#fish data
+
+nothing = "Nothing(+0)"
+
+worthless = "Trash(+1)"
+
+common = "a Catfish!(+5)"
+
+uncommon = "a Seabass!(+15)"
+
+rare = "a Manta Ray!(+25)"
+
+ultrarare = "a Shark!!(+50)"
+
+godly = "a Whale..?!(+75)"
+
+mythic = "a...Ferret..?(+150)"
+
+
+fish_types = [nothing, worthless, common, uncommon, rare, ultrarare, godly, mythic]
